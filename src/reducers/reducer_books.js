e8c71c03a14ba35ceca2d37d1385d479fbad0950
Michael Grienauer <mgrienauer@gmail.com>
2018-10-10 12:55:09 +0200

    Update reducer for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return {
+        [
+            { title: 'JS: the good parts' },
+            { title: 'Harry Potter' },
+            { title:'The Dark tower '},
+            { title:'Blood Meridian' }
+        ]
+    }
+}
